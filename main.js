4bccb834c54787cf3ee69086df8338879e0b3ac4
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-04-16 14:18:04 -0400

    Updated querySelector project files

---
+++ main.js
@@ -0,0 +1,19 @@
+const student = document.getElementById('student');
+const sendingschool = document.querySelector('#sendingschool');
+const city = document.querySelector('#city');
+
+document.body.style.backgroundColor = "lightblue";
+
+student.style.color = '#cc1075';
+student.style.fontSize = '1.7rem';
+
+sendingschool.style.fontSize = '1.2rem';
+sendingschool.style.marginBottom = '30px';
+sendingschool.style.fontFamily = 'Helvetica, sans-serif';
+sendingschool.style.fontVariant = 'small-caps';
+sendingschool.style.fontStyle = 'italic';
+
+city.style.display = 'inline';
+city.style.color = 'salmon';
+city.style.padding = '10px';
+city.style.border = '4px dotted orange';
